48b02203759b3916f83952a618ba30bd2315a026
yara <yara2002@pisem.net>
2021-06-26 16:38:27 +0400

    added exercise for module 9

--- src/Loader.java
+++ src/Loader.java
@@ -1,25 +1,23 @@
-import person.Person;
-
-import java.text.ParseException;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Loader {
-    public static void main(String[] args) throws ParseException {
-        Person p1 = new Person("Василий Пупкин", "25.03.2001");
-        Person p2 = new Person("Иван Иванович Иванов", "01.08.1965");
-        Person p3 = new Person("Петр Петрович Петров", "25.02.1978");
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/file.txt");
+        File zipFile = new File("src/output.zip");
 
-        Person[] persons = {p1, p2, p3};
+        InputStream input = new FileInputStream(file);
+        ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipFile));
 
-        Arrays.sort(persons, new Comparator<>() {
-            @Override
-            public int compare(Person p1, Person p2) {
-                return p2.getBirthday().compareTo(p1.getBirthday());
-            }
-        });
+        zipOut.putNextEntry(new ZipEntry(file.getName()));
+        zipOut.setComment("This is a zip file.");
+        int temp;
+        while ((temp = input.read()) != -1) {
+            zipOut.write(temp);
+        }
 
-        for (Person p: persons)
-            System.out.println(p);
+        input.close();
+        zipOut.close();
     }
 }
