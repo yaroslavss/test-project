1fe4fbac3d1b5878aa372cc8c10ac84aed1d9a23
yara <yara2002@pisem.net>
2021-07-08 13:05:25 +0400

    added exercise for module 10

--- src/Loader.java
+++ src/Loader.java
@@ -3,21 +3,20 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Loader {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         File file = new File("src/file.txt");
         File zipFile = new File("src/output.zip");
 
-        InputStream input = new FileInputStream(file);
-        ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipFile));
-
-        zipOut.putNextEntry(new ZipEntry(file.getName()));
-        zipOut.setComment("This is a zip file.");
-        int temp;
-        while ((temp = input.read()) != -1) {
-            zipOut.write(temp);
+        try (InputStream input = new FileInputStream(file);
+             ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipFile))) {
+            zipOut.putNextEntry(new ZipEntry(file.getName()));
+            zipOut.setComment("This is a zip file.");
+            int temp;
+            while ((temp = input.read()) != -1) {
+                zipOut.write(temp);
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка архивирования: " + e.getMessage());
         }
-
-        input.close();
-        zipOut.close();
     }
 }
